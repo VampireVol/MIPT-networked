b34cc7771df2f5e797a6b341e85aa4f7d41c4600
Sergey Balanin <sergeyb11@ya.ru>
2022-03-29 23:28:43 +0300

    add hw-1

--- hw-1/client.cpp
+++ hw-1/client.cpp
@@ -2,15 +2,34 @@
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <netdb.h>
+#include <string>
 #include <cstring>
 #include <cstdio>
 #include <iostream>
+#include <vector>
 #include "socket_tools.h"
+
+enum State {
+    INIT,
+    VAR1,
+    VAR2,
+    VAR3
+};
+
+const std::string initStr = "0 ";
+const std::string messageStr = "1 ";
+const std::string keepAliveStr = "2 ";
+
+std::vector<std::string> words1 { "Hello World!", "My message", "I started at " + std::to_string(time(nullptr)), "Some another message" };
+std::vector<std::string> words2 { "Message #1", "Message #2", "Message #3", "Message #4", "Message #5" };
+std::vector<std::string> words3 { "Test Message #1", "Test Message #2", "Test Message #3", "Test Message #4", "Test Message #5" };
 
 int main(int argc, const char **argv)
 {
+  srand(time(nullptr));
   const char *port = "2022";
-
+  State state = INIT;
+  std::string userName;
   addrinfo resAddrInfo;
   int sfd = create_dgram_socket("localhost", port, &resAddrInfo);
 
@@ -20,14 +39,73 @@ int main(int argc, const char **argv)
     return 1;
   }
 
+  uint32_t timeStart = time(nullptr);
+  uint32_t lastSendTime = timeStart;
+  uint32_t lastKeepAliveTime = timeStart;
+
+  std::string output;
+  printf("Hi what your name?\n");
+  std::getline(std::cin, userName);
+  printf("Choose message variant 1 or 2\n");
+  std::getline(std::cin, output);
+  if (output == "1")
+    state = VAR1;
+  else if (output == "2")
+    state = VAR2;
+  else {
+    printf("Time to use variant 3 :)\n");
+    state = VAR3;
+  }
+  output = initStr + userName;
+  ssize_t res = sendto(sfd, output.c_str(), output.size(), 0, resAddrInfo.ai_addr, resAddrInfo.ai_addrlen);
+  if (res == -1)
+    std::cout << hstrerror(errno) << std::endl;
+
   while (true)
   {
-    std::string input;
-    printf(">");
-    std::getline(std::cin, input);
-    ssize_t res = sendto(sfd, input.c_str(), input.size(), 0, resAddrInfo.ai_addr, resAddrInfo.ai_addrlen);
-    if (res == -1)
-      std::cout << strerror(errno) << std::endl;
+    uint32_t curTime = time(nullptr);
+    if (curTime - lastSendTime > 0)
+    {
+      lastSendTime = curTime;
+      std::string input;
+      std::string inputs;
+      switch (state)
+      {
+        case VAR1:
+          input = words1[rand() % words1.size()];
+          output = messageStr + userName + " " + input;
+          break;
+        case VAR2:
+          input = words2[rand() % words2.size()];
+          output = messageStr + userName + " " + input;
+          break;
+        case VAR3:
+          input = words3[rand() % words3.size()];
+          output = messageStr + userName + " " + input;
+          break;
+      }
+      printf(">%s \n", input.c_str());
+      ssize_t res = sendto(sfd, output.c_str(), output.size(), 0, resAddrInfo.ai_addr, resAddrInfo.ai_addrlen);
+      if (res == -1)
+        std::cout << hstrerror(errno) << std::endl;
+    }
+    if (curTime - lastKeepAliveTime > 4)
+    {
+      lastKeepAliveTime = curTime;
+      output = keepAliveStr + userName;
+      ssize_t res = sendto(sfd, output.c_str(), output.size(), 0, resAddrInfo.ai_addr, resAddrInfo.ai_addrlen);
+      if (res == -1)
+        std::cout << hstrerror(errno) << std::endl;
+    }
+
+    constexpr size_t buf_size = 1000;
+    static char buffer[buf_size];
+    std::memset(buffer, 0, buf_size);
+    ssize_t numBytes = recvfrom(sfd, buffer, buf_size - 1, 0, nullptr, nullptr);
+    if (numBytes > 0)
+    {
+      printf("%s\n", buffer);
+    }
   }
   return 0;
 }
--- hw-1/server.cpp
+++ hw-1/server.cpp
@@ -5,7 +5,39 @@
 #include <cstring>
 #include <cstdio>
 #include <iostream>
+#include <vector>
 #include "socket_tools.h"
+
+
+struct UserInfo {
+    std::string name;
+    struct sockaddr from;
+    uint32_t addrlen;
+};
+
+std::vector<UserInfo> info;
+
+int tryFindUser(const std::string &name)
+{
+  for (int i = 0; i < info.size(); ++i)
+  {
+    if (name == info[i].name)
+      return i;
+  }
+  return -1;
+}
+
+void sendMessageFromUser(int sfd, const std::string &name, const std::string &message)
+{
+  for (int i = 0; i < info.size(); ++i)
+  {
+    if (name != info[i].name)
+    {
+      std::string output = name + ": " + message;
+      sendto(sfd, output.c_str(), output.size(), 0, &info[i].from, info[i].addrlen);
+    }
+  }
+}
 
 int main(int argc, const char **argv)
 {
@@ -29,13 +61,54 @@ int main(int argc, const char **argv)
 
     if (FD_ISSET(sfd, &readSet))
     {
+      struct sockaddr from;
+      uint32_t addrlen;
       constexpr size_t buf_size = 1000;
       static char buffer[buf_size];
       memset(buffer, 0, buf_size);
-
-      ssize_t numBytes = recvfrom(sfd, buffer, buf_size - 1, 0, nullptr, nullptr);
+      addrlen = sizeof(from);
+      ssize_t numBytes = recvfrom(sfd, buffer, buf_size - 1, 0, &from, &addrlen);
       if (numBytes > 0)
-        printf("%s\n", buffer); // assume that buffer is a string
+      {
+        char* name;
+        char* code;
+        char* save;
+        code = strtok_r(buffer, " ", &save);
+        name = strtok_r(nullptr, " ", &save);
+        if (buffer[0] == '0')
+        {
+          int idx = tryFindUser(std::string(name));
+          if (idx == -1)
+          {
+            printf("Add new user: %s\n", name);
+            info.push_back({ std::string(name), from, addrlen });
+          }
+          else
+          {
+            printf("User info updated: %s\n", name);
+            info[idx].from = from;
+            info[idx].addrlen = addrlen;
+          }
+        }
+        else if (buffer[0] == '1')
+        {
+          printf("From: %s get message: %s \n", name, save);
+          sendMessageFromUser(sfd, std::string(name), save);
+        }
+        else if (buffer[0] == '2')
+        {
+          printf("Get keep alive packet from: %s\n", name);
+        }
+        else
+        {
+          printf("Read invalid message: %s\n", buffer);
+        }
+
+        //printf("%s %d \n", buffer, addrlen); // assume that buffer is a string
+        //memmove(buffer, buffer + 2, strlen(buffer));
+        //printf("new buffer: %s\n", buffer);
+      }
+
     }
   }
   return 0;
